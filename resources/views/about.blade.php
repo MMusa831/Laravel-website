e45e14a4a295149b7d468760301d077b83f43220
Mohamed Musa <mohamed.musa831@gmail.com>
2023-06-17 16:29:58 +0200

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <title>About Page</title>
+</head>
+<body>
+  <h1>This is about Page!!!!</h1>
+  {{-- <a href="{{ route('contact') }}">Contact</a>   --}}
+  <a href="{{ url('/contact') }}">Contact</a>  
+</body>
+</html>
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+
+<img src="{{ asset('logo/star.png') }}" width="100px">
+
